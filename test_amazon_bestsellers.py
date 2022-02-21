e90a77b99407b116bc5a0971ed4d5b4c2a58ec0c
Ilie Alex <62673494+IlieAlexandru-Ioan@users.noreply.github.com>
2022-02-21 18:44:37 +0200

    Update test_amazon_bestsellers.py

--- test_amazon_bestsellers.py
+++ test_amazon_bestsellers.py
@@ -12,9 +12,9 @@ class TestAmazonBestSellers:
 
     def test_best_sellers(self):
         self.driver.find_element(By.XPATH,"//div[@id='nav-xshop']/a[contains(@href, 'gold')]").click()
-        #find element va returna o eroare dc nu il gaseste iar find elements nu va returna o eroare dc nu gaseste niste elemente
-        # actual_links = self.driver.find_elements(By.XPATH, "//div[@id='nav-xshop']/a")
-        # assert len(actual_links) == 5, f"Expected 5 but found {len(actual_links)}"
+        #find element va returna o eroare dc nu il gaseste iar find elements nu va returna o eroare dc nu gaseste elemente
+         actual_links = self.driver.find_elements(By.XPATH, "//div[@id='nav-xshop']/a")
+         assert len(actual_links) == 5, f"Expected 5 but found {len(actual_links)}"
 
     def teardown_method(self):
         self.driver.quit()
